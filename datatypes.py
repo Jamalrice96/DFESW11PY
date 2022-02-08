073085540dc2a9d0d955dde306cc162424b05c96
jamalrice96 <jamal.rice96@gmail.com>
2022-02-08 12:42:03 +0000

    commit

--- datatypes.py
+++ datatypes.py
@@ -1,11 +1,14 @@
 var1 = 3
 var1a = 3.5
-var2 = "jamal"
-var2a = "rice"
-Var3 = True
-Var3a = False
-       #Mathematical sum
-print (var2 + var2a)           #String Concatenation
+var2 = "Jamal"
+var2a = "Rice"
+var2b = "4"
+var3 = True
+var3a = False
 
-print (var1 + var1a)
+print(var1 + var1a)  # mathematical sum
+print(var2 + var2a)  # string concatenation
+print(var1 + int(var2b))
+print(str(var1) + var2a)
 
+print(type(var3))
