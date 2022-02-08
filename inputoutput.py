6da5ddf16c80f46707335e0ca19e9546fddad5be
jamalrice96 <jamal.rice96@gmail.com>
2022-02-08 12:56:05 +0000

    commit

---
+++ inputoutput.py
@@ -0,0 +1,32 @@
+# var4 = int(input("Type in a number I will double it: "))
+# print(type(var4))
+# print(var4 * 2)   # this is output
+
+
+# number1 = float(input("Please enter the first number: "))
+# number2 = float(input("Please enter the second number: "))
+# answer = number1 + number2
+
+# print(number1, "+", number2, "=", answer)
+
+
+# var1 = 121010               # Integer
+# var1a = 10.00               # Float
+# var2 = "This is a string"
+# var2a = 'this is also a string'
+
+
+# print(str(var1) + var2a)
+# print(var1 , var2a)
+# print(f"here is the first variable:{var1}and here is the second variable: {var2}")
+
+
+
+# var3 = ""
+# print(bool(var3))
+
+print('jamz\'s a fire bender')
+print("Today is 01\\01\\2001")
+print("This is a school for the fire element \you are not welcome")
+
+print("🔥")
