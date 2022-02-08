6ef1212df5f00b5aabc9e9914d216f827422a77b
jamalrice96 <jamal.rice96@gmail.com>
2022-02-08 12:17:36 +0000

    commit

---
+++ collections.py
@@ -0,0 +1,21 @@
+listvar1=   ["fruit", "yoghurt", "wine", "rum for the weekend"]
+#                0            1        2         3    (binary index)
+print(listvar1)
+
+cool_cows = ["Winnie the Moo", "Moolan", "Milkshake", "Mooana"]
+cool_sheep = ["Baaaart", "Baaaarnaby"]
+cool_pigs = ["Chris P. Bacon", "Hamlet", "Hogwarts"]
+
+cool_animals = [cool_cows, cool_sheep, cool_pigs]
+
+print (cool_animals[2][0])
+
+listvar1.append("painkillers")
+print(listvar1)
+
+listvar2= ("meat")
+print (listvar2)
+
+
+listvar3 = {"yo" : "wazup", "boo" : "baa"}
+print(listvar3[
---
+++ dictionaries.py
@@ -0,0 +1,10 @@
+dict={"name": "Plato", "country": "Ancient Greece", "born": -427, "teacher": "Socrates", "student": "Aristotle"}
+answer_1=dict["born"]
+print(answer_1)
+
+dict={"name": "Plato", "country": "Ancient Greece", "born": -427, "teacher": "Socrates", "student": "Aristotle"}
+dict["born"]= -428
+
+
+dict["work"] = ["Apology", "Phaedo", "Republic", "Symposium"]
+print(dict)
