c3a7993fcc29b2a2062ea961a02ea10d91d08fc5
jamalrice96 <jamal.rice96@gmail.com>
2022-02-09 09:22:40 +0000

    commit

--- datatypes.py
+++ datatypes.py
@@ -5,9 +5,8 @@ var2a = "Rice"
 var2b = "4"
 var3 = True
 var3a = False
-
-print(var1 + var1a)  # mathematical sum
-print(var2 + var2a)  # string concatenation
+print(var1 + var1a)  # math
+print(var2 + var2a)  # string
 print(var1 + int(var2b))
 print(str(var1) + var2a)
 
---
+++ math.py
@@ -0,0 +1,15 @@
+# Sample code
+
+nl=[]
+for x in range(1500, 2701):
+    if (x%7==0) and (x%5==0):
+        nl.append(str(x))
+print (','.join(nl))
+
+
+
+nl=[]
+for x in range (1200, 1900):
+    if (x%20==0) and (x%10==0):
+        nl.append(str(x))
+        print (','.join(nl))
